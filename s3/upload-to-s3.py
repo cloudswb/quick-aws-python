81ef1aa6c774dbac66a347ddd07f6e08804bbc77
思归 <zk.life@qq.com>
2023-02-13 09:21:20 +0800

    Operqating the s3 using a specific profile name.

--- s3/upload-to-s3.py
+++ s3/upload-to-s3.py
@@ -11,10 +11,10 @@ my_config = Config(
     # signature_version = 'v4'
 )
 
+# Initialize a s3 client object using a specific profile name.
+# refer to : https://boto3.amazonaws.com/v1/documentation/api/latest/guide/credentials.html
 session = boto3.Session(profile_name='19873900')
 s3 = session.client('s3')
-# with open("FILE_NAME", "rb") as f:
-#     s3.upload_fileobj(f, "BUCKET_NAME", "OBJECT_NAME")
 
 
 def list_all_buckets():
@@ -25,8 +25,15 @@ def list_all_buckets():
     for bucket in response['Buckets']:
         print(f'  {bucket["Name"]}')
 
+def list_bucket_files():
+    
+    response = s3.list_objects(Bucket='zkdoc', MaxKeys=2)
 
-def upload_file(file_name, bucket, object_name=None):
+    for my_bucket_object in response['Contents']:
+        print(my_bucket_object['Key'])
+
+
+def upload_file_to_bucket(file_name, bucket, object_name=None):
     """Upload a file to an S3 bucket
 
     :param file_name: File to upload
@@ -35,20 +42,27 @@ def upload_file(file_name, bucket, object_name=None):
     :return: True if file was uploaded, else False
     """
 
-    # If S3 object_name was not specified, use file_name
-    if object_name is None:
-        object_name = os.path.basename(file_name)
+    s3.upload_file(file_name, bucket, object_name)       
 
-    # Upload the file
-    s3_client = boto3.client('s3', config=my_config)
-    try:
-        response = s3_client.upload_file(file_name, bucket, object_name)
-    except ClientError as e:
-        logging.error(e)
-        return False
-    return True
+def delete_object_from_bucket(bucket, object_name=None):
+    """Delete a file from an S3 bucket
+
+    :param bucket: Bucket to delete from.
+    :param object_name: S3 object name.
+    :return: True if file was uploaded, else False
+    """
+    response = s3.delete_object(Bucket='zkdoc', Key='Tech_Transcripts_reInvent_Combined.pdf')
+    print(response)
 
 
+
+
+# Bucket operations refer to: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html#S3.Client.delete_object
+# List all buckets using the specific profile credentional.
 list_all_buckets()
+# list fils in bucket.
 list_bucket_files()
-upload_file_to_bucket()
+# upload one file to bucket.
+upload_file_to_bucket('/Users/zhangkap/Downloads/Tech_Transcripts_reInvent_Combined.pdf', 'zkdoc', 'Tech_Transcripts_reInvent_Combined.pdf')
+# delete one object from the specific bucket.
+delete_object_from_bucket('Tech_Transcripts_reInvent_Combined.pdf', 'zkdoc')
